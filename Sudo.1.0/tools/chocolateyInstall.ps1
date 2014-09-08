0fe6eba7eb782fd4c09568e1800114e71174636d
Jan Hebnes <jan.hebnes@gmail.com>
2014-09-08 11:02:11 +0200

    Tested and added manual removal of old Shim file if present

--- Sudo.1.0/tools/chocolateyInstall.ps1
+++ Sudo.1.0/tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 try{
-  Generate-BinFile 'Sudo' '..\lib\Sudo.1.0\bin\sudo.cmd'
+  Remove-BinFile 'Sudo' '..\lib\Sudo.1.0\bin\sudo.cmd'
+  Generate-BinFile 'Sudo' '..\lib\Sudo.1.0.1\bin\sudo.cmd'
   Write-ChocolateySuccess 'sudo'
 } catch {
   Write-ChocolateyFailure 'sudo' "$($_.Exception.Message)"
